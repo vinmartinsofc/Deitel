220ec5b7e731b65d4188cc863fdde8daa045da88
Vin Martins <marcosviniciossl3@gmail.com>
2026-05-05 21:25:18 -0300

    update: understanding for completely

--- src/Random/Test.java
+++ src/Random/Test.java
@@ -1,23 +1,16 @@
 package Random;
 
-import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int i = (int) (Math.random() * 11);
 
-        System.out.println("Aaaa");
-        int bbbbb = scanner.nextInt();
 
-        while (scanner.hasNext()) {
-
-            System.out.println("I entered: " + bbbbb);
-
-            System.out.println("Aaaa");
-             bbbbb = scanner.nextInt();
+        if (i % 2 == 0) {
+            System.out.println("Even");
+        } else  {
+            System.out.println("Odd");
         }
-
-
 
 
     }
---
+++ src/UnderstandingFor/Ex01.java
@@ -0,0 +1,21 @@
+package UnderstandingFor;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("*");
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+
+        }
+
+
+    }
+
+
+}
