42cb3e6497c79bef0d7374af049adb01828adb8e
Vin Martins <marcosviniciossl3@gmail.com>
2026-05-31 01:28:35 -0300

    update: resume book

---
+++ src/Tests/ArrayTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] n = { 10, 20, 30, 40, 50 };
+
+        for (int i : n) {
+            System.out.println(i);
+        }
+    }
+}
