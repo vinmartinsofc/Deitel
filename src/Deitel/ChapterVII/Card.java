c9a457b22f7844aa66a4304399de54c7a75707b3
Vin Martins <marcosviniciossl3@gmail.com>
2026-06-04 00:24:37 -0300

    update: new linux distro

---
+++ src/Deitel/ChapterVII/Card.java
@@ -0,0 +1,16 @@
+package Deitel.ChapterVII;
+
+public class Card {
+    private final String face;
+    private final String suit;
+
+    public Card(String cardFace, String cardSuit) {
+        this.face = cardFace;
+        this.suit = cardSuit;
+    }
+
+    @Override
+    public String toString() {
+        return face + "of" + suit;
+    }
+}
